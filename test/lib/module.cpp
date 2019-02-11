1a551335d1f5a5f853fd60d1b90acbe8fb013392
Michele Caini <michele.caini@gmail.com>
2019-02-11 15:09:40 +0100

    added lib_export

--- test/lib/module.cpp
+++ test/lib/module.cpp
@@ -1,12 +1,22 @@
 #include <entt/entity/registry.hpp>
 
-typename entt::registry<>::component_type int_type() {
+#ifndef LIB_EXPORT
+#if defined _WIN32 || defined __CYGWIN__
+#define LIB_EXPORT __declspec(dllexport)
+#elif defined __GNUC__
+#define LIB_EXPORT __attribute__((visibility("default")))
+#else
+#define LIB_EXPORT
+#endif
+#endif
+
+LIB_EXPORT typename entt::registry<>::component_type int_type() {
     (void)entt::registry<>::type<double>();
     (void)entt::registry<>::type<float>();
     return entt::registry<>::type<int>();
 }
 
-typename entt::registry<>::component_type char_type() {
+LIB_EXPORT typename entt::registry<>::component_type char_type() {
     (void)entt::registry<>::type<double>();
     (void)entt::registry<>::type<float>();
     return entt::registry<>::type<char>();
